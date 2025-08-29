8c8a7ae661b91de103358e1d48a2d09665926f28
Siddharth Jain <sidojain30705@gmail.com>
2025-08-29 18:39:18 +0530

    update .env

--- api/server.py
+++ api/server.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
-import redis
+from upstash_redis import Redis
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-redis_client = redis.Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:5000"))
+redis_client = Redis(url=os.getenv("REDIS_URL", ""), token=os.getenv("REDIS_TOKEN", ""))
 
 HIGH_SCORE_KEY = "tetris_highscore"
 
 @app.route('/highscore', methods=['GET'])
-async def get_highscore():
-    score = await redis_client.get(HIGH_SCORE_KEY)
+def get_highscore():
+    score = redis_client.get(HIGH_SCORE_KEY)
     if score is None:
         score = 0
     else:
@@ -17,15 +19,15 @@ async def get_highscore():
     return jsonify({"highscore": score})
 
 @app.route('/highscore', methods=['POST'])
-async def update_highscore():
-    data = await request.get_json()
+def update_highscore():
+    data = request.get_json()
     new_score = data.get("score", 0)
-    current_score = await redis_client.get(HIGH_SCORE_KEY)
+    current_score = redis_client.get(HIGH_SCORE_KEY)
     if current_score is None:
         current_score = 0
     else:
         current_score = int(current_score)
     if isinstance(new_score, int) and new_score > current_score:
-        await redis_client.set(HIGH_SCORE_KEY, new_score)
+        redis_client.set(HIGH_SCORE_KEY, new_score)
         current_score = new_score
     return jsonify({"highscore": current_score})
